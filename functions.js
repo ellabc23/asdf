8608572042ee8ea3d6e557f0835e0a52af3fa57b
Ella <33379153+ellabc23@users.noreply.github.com>
2019-01-19 11:23:21 -0800

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,13 @@
+var count=0;
+
+function like() {
+  count +=1;
+updateUI();
+}
+function dislike() {
+  count -=1;
+updateUI();
+}
+function updateUI() {
+ document.getElementById('count').innerHTML=count;
+}
